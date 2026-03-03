14210b4984a6aa8a980b1bde68c37323b5250c41
Babacar Diouf <babacar.mandiouf.diouf@gmail.com>
2026-03-03 13:02:25 +0000

    Correction edit.php production

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 <div class="py-12">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div class="w-full px-4 sm:px-6 lg:px-8 py-8">
         <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
             <div class="p-6 bg-white border-b border-gray-200">
                 <h2 class="text-2xl font-bold text-gray-800 mb-6">
