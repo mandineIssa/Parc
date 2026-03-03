02ca2df3a9b4ffca1739987e66778625995fc8c0
Babacar Diouf <babacar.mandiouf.diouf@gmail.com>
2026-03-03 14:22:31 +0000

    Correction sur audit production

--- resources/views/audits/index.blade.php
+++ resources/views/audits/index.blade.php
@@ -4,7 +4,7 @@
 @section('header', 'Journal des Activités')
 
 @section('content')
-<div class="container mx-auto px-4 py-8">
+<div class="w-full px-4 sm:px-6 lg:px-8 py-8">
     <!-- En-tête -->
     <div class="flex flex-col md:flex-row justify-between items-start md:items-center mb-8">
         <div>
