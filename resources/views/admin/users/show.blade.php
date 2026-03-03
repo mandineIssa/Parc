5cee93228ab6457d395cfe0b5162d908a3560c08
Babacar Diouf <babacar.mandiouf.diouf@gmail.com>
2026-03-03 13:00:07 +0000

    Correction index.php production

--- resources/views/admin/users/show.blade.php
+++ resources/views/admin/users/show.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 <div class="py-12">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div class="w-full px-4 sm:px-6 lg:px-8 py-8">
         <div class="flex justify-between items-center mb-6">
             <h2 class="text-2xl font-bold text-gray-800">
                 {{ __('Détails de l\'utilisateur') }}
